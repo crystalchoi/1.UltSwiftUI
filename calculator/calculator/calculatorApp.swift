bb5081f474fcf4f089cd65f3dcde359c8e7bd787
crystalchoi <sujeungchoi@gmail.com>
2025-08-19 01:45:07 +0900

    init

---
+++ calculator/calculator/calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorApp.swift
+//  calculator
+//
+//  Created by crystalmac on 8/19/25.
+//
+
+import SwiftUI
+
+@main
+struct calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
